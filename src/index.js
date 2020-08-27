612fc13145cd656be145d86a10217eb3f5cfc41b
Mark Edward Ratio <markedwardratio@gmail.com>
2020-08-27 13:45:34 +0800

    Add projects and tasks UI (incomplete)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import WebpackImg from './webpack.png';
-
-function component() {
-  const div = document.createElement('div');
-
-  const p = document.createElement('p');
-  p.textContent = 'Webpack Test';
-  div.appendChild(p);
-
-  const img = new Image();
-  img.src = WebpackImg;
-  div.appendChild(img);
-
-  return div;
-}
-
-document.body.appendChild(component());
+import './style.css';
